61098f410b3494ae3d4719694e7af04908e522c5
dan-te6741 <danieli96@nycstudents.net>
2026-01-29 10:11:06 -0500

    rectangle and triangle

--- 67.py
+++ 67.py
@@ -6,17 +6,42 @@ t = Turtle()
 
 # turtle.done()
 
-t.shape('turtle')
+# t.shape('turtle')
 
-def square(x):
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
+# def square(x):
+#     t.forward(x)
+#     t.left(90)
+#     t.forward(x)
+#     t.left(90)
+#     t.forward(x)
+#     t.left(90)
+#     t.forward(x)
+#     t.left(90)
     
-square(200)
+# square(200)
+# turtle.done()
+
+# def rectangle(x,y):
+#     t.forward (x)
+#     t.left (90)
+#     t.forward(y)
+#     t.left(90)
+#     t.forward(x)
+#     t.left(90)
+#     t.forward(y)
+#     t.left(90)
+
+# rectangle(100,125)
+
+# turtle.done()
+
+def triangle(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.right(120)
+
+triangle(90)
 turtle.done()
