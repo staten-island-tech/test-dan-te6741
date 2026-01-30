7f8fa89b2a5e023f23607addf125299a0a82f650
dan-te6741 <danieli96@nycstudents.net>
2026-01-30 10:17:24 -0500

    dayidkiforgot

---
+++ 68.py
@@ -0,0 +1,54 @@
+import turtle
+from turtle import *
+t = Turtle()
+
+
+# def squaredef():
+#     t.forward(50)
+#     t.left(90)
+#     t.forward(50)
+#     t.left(90)
+#     t.forward(50)
+#     t.left(90)
+#     t.forward(50)
+#     t.left(90)
+
+# def square():
+#     for i in range (60):
+#         squaredef()
+
+# square()
+
+# turtle.done()
+
+# for i in range (3):
+#  t.forward(100)
+#  t.left(120)
+
+# turtle.done()
+
+
+
+def squaredef():
+    t.forward(50)
+    t.left(90)
+    t.forward(50)
+    t.left(90)
+    t.forward(50)
+    t.left(90)
+    t.forward(50)
+    t.left(90)
+    t.right(5)
+
+def square():
+    for i in range (60):
+        squaredef()
+
+square()
+
+turtle.done()
+
+
+
+    
+
